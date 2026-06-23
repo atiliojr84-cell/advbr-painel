809b77abbdc236b522c71a243e02564b56f69d45
atiliojr84-cell <atilio.jr84@gmail.com>
2026-06-23 16:20:18 -0300

    Update status-atual.js

--- api/status-atual.js
+++ api/status-atual.js
@@ -6,14 +6,13 @@ const kv = createClient({
 });
 
 export default async function handler(req, res) {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-
-    try {
-        const dados = await kv.get('advbr_status_global');
-        return res.status(200).json(dados || {});
-    } catch (error) {
-        return res.status(500).json({ erro: "Erro ao buscar do Redis." });
-    }
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  try {
+    const dados = await kv.get('advbr_status_global');
+    return res.status(200).json(dados || {});
+  } catch (error) {
+    return res.status(500).json({ erro: 'Erro ao buscar do Redis.' });
+  }
 }
