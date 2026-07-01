ba1e40633cfc4d8ba1bd480e0a412798a1377dc3
atiliojr84-cell <atilio.jr84@gmail.com>
2026-07-01 19:03:24 -0300

    Create mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,41 @@
+// lib/mongodb.ts
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("Please add your MONGODB_URI to .env.local");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the client
+  // is not re-created on every hot-reload
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient object. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
