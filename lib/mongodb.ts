039e4b7196eed2e4ac7ef194ce8d81654bd5961c
atiliojr84-cell <atilio.jr84@gmail.com>
2026-07-02 03:02:27 -0300

    Atualizar o mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,11 +3,8 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
-  throw new Error("A variável de ambiente MONGODB_URI não está definida.");
+  throw new Error("MONGODB_URI não definida nas variáveis de ambiente.");
 }
-
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
 const options = {
   serverApi: {
@@ -15,17 +12,17 @@ const options = {
     strict: true,
     deprecationErrors: true,
   },
-  connectTimeoutMS: 10000, // Timeout de 10s para evitar travamentos
 };
+
+let client: MongoClient = new MongoClient(uri, options);
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!(global as any)._mongoClientPromise) {
-    client = new MongoClient(uri, options);
     (global as any)._mongoClientPromise = client.connect();
   }
   clientPromise = (global as any)._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
